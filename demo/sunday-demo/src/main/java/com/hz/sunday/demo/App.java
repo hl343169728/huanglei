f404893114e0dcc648fc2fe9877d5a921f44f2d1
hl4098946 <hl343169728>
2015-06-10 14:28:01 +0800

    我的测试

---
+++ demo/sunday-demo/src/main/java/com/hz/sunday/demo/App.java
@@ -0,0 +1,13 @@
+package com.hz.sunday.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
